1b5cacc4bf9fa130ecd4c517c7976541a4240e10
anilkivanc <anilkivanc01@gmail.com>
2025-06-02 04:27:50 +0300

    login test

--- mentup-frontend/src/config.js
+++ mentup-frontend/src/config.js
@@ -1,5 +1,2 @@
-const isProduction = process.env.NODE_ENV === "production";
+export const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5001";
 
-export const API_BASE_URL = isProduction
-  ? process.env.REACT_APP_API_URL_PROD
-  : process.env.REACT_APP_API_URL;
--- mentup-frontend/src/pages/Login/Login.js
+++ mentup-frontend/src/pages/Login/Login.js
@@ -23,7 +23,7 @@ const Login = () => {
     setMessage("");
 
     try {
-      const response = await fetch("http://localhost:5001/auth/login", {
+      const response = await fetch(`${API_BASE_URL}/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
