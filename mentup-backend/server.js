b136c1fc9ce94b3640086c7b3d1ab0991b5ae378
anilkivanc <anilkivanc01@gmail.com>
2025-06-02 20:27:06 +0300

    Update server.js

--- mentup-backend/server.js
+++ mentup-backend/server.js
@@ -13,6 +13,9 @@ const PORT = process.env.PORT || 5001; // .env dosyasındaki PORT'u kullanır
         // Sunucuyu başlat
         app.listen(PORT, () => {
             console.log(`Server running on http://localhost:${PORT}`);
+            console.log("NODE_ENV:", process.env.NODE_ENV);
+            console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
         });
     } catch (error) {
         console.error('Error synchronizing the database:', error);
