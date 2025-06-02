80524c8e8aa2fc91b0424418f23c0acd2b7f0c13
anilkivanc <anilkivanc01@gmail.com>
2025-06-02 20:34:55 +0300

    Update database.js

--- mentup-backend/src/config/database.js
+++ mentup-backend/src/config/database.js
@@ -1,9 +1,6 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
 const isProduction = process.env.NODE_ENV === 'production';
 const connectionString = isProduction
   ? process.env.DATABASE_URL
